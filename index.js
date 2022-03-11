5bf813cc148372dfd6a586abf18ab56c79310e37
nygder <34347964+nygder@users.noreply.github.com>
2022-03-11 17:46:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+[Application Options]
+rpcuser=myuser
+rpcpass=SomeDecentp4ssw0rd
+datadir=D:/btcd/data
+logdir=D:/btcd/logs
+configfile=D:/btcd/config/btcd.conf
+rpccert=D:/btcd/config/rpc.cert
+rpckey=D:/btcd/config/rpc.key
+debuglevel=debug
